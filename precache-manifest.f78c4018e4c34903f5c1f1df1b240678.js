edab03bd56e0ccc55fbdc3276e6211a7e851a150
Junaid Amin <junaid.amin@gexton.com>
2019-02-04 15:06:12 +0500

    Updates

---
+++ precache-manifest.f78c4018e4c34903f5c1f1df1b240678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bdd91ccb2d01f4ef4d0",
+    "url": "/react-contacts/static/js/runtime~main.5bdd91cc.js"
+  },
+  {
+    "revision": "135f0b26ff480ca42a33",
+    "url": "/react-contacts/static/js/main.135f0b26.chunk.js"
+  },
+  {
+    "revision": "21fd4cc44b11acb28d9b",
+    "url": "/react-contacts/static/js/1.21fd4cc4.chunk.js"
+  },
+  {
+    "revision": "135f0b26ff480ca42a33",
+    "url": "/react-contacts/static/css/main.fdb58714.chunk.css"
+  },
+  {
+    "revision": "21fd4cc44b11acb28d9b",
+    "url": "/react-contacts/static/css/1.153e2dfd.chunk.css"
+  },
+  {
+    "revision": "21529df1a2a095fea85273c6c78c710f",
+    "url": "/react-contacts/index.html"
+  }
+];
